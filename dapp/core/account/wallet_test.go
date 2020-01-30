816b8975dd9377ccbd47a4aef2b4bb9f2f555c5f
proxeus-dev <proxeus-dev@proxeus.com>
2020-01-30 16:29:34 +0100

    Add wallet login/logout test

---
+++ dapp/core/account/wallet_test.go
@@ -0,0 +1,43 @@
+package account
+
+import (
+	"github.com/ProxeusApp/storage-app/dapp/core/embdb"
+	"github.com/ethereum/go-ethereum/common"
+	"testing"
+)
+
+func TestWallet_LoginWithNewAccount(t *testing.T) {
+	wallet := &Wallet{walletUsageDB: embdb.OpenDummyDB()}
+	err := wallet.LoginWithNewAccount("jesse", "securePassword")
+	if err != nil {
+		t.Error(err)
+	}
+
+	if wallet.activeAcc.name != "jesse" {
+		t.Error("Expected active account to be 'jesse' but got: ", wallet.activeAcc.name)
+	}
+
+	accFileList := wallet.All()
+	if len(accFileList) != 1 {
+		t.Error("Expected to find 1 account but got ", len(accFileList))
+	}
+
+	if accFileList[0].Account().name != "jesse" {
+		t.Error("Expected active account to be 'jesse' but got: ", accFileList[0].Account().name)
+	}
+}
+
+func TestWallet_Logout(t *testing.T) {
+	wallet := &Wallet{}
+	addr := common.HexToAddress("0xa80899bb12e4afe9787425a5e5fe166234b88185")
+	wallet.activeAcc = &Account{ethAddr: &addr}
+
+	err := wallet.Logout()
+	if err != nil {
+		t.Error(err)
+	}
+
+	if wallet.activeAcc != nil {
+		t.Errorf("Expetec activeAcc to be nil")
+	}
+}
